680e332966fd7cd9694f7d5f8e98914f2ff9b633
sintadianti <1606009@sttgarut.ac.id>
2020-06-13 11:01:28 +0700

    naon we

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
+<?php 
+  $page=isset($_GET["page"])?$_GET["page"]:'dashboard';
+ ?>
 <!DOCTYPE html>
 <html lang="en">
+
   
 <!-- Mirrored from big-bang-studio.com/cosmos/pages-blank.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 10 Jan 2019 12:55:36 GMT -->
 <head>
@@ -14,6 +18,8 @@
     <link rel="stylesheet" href="css/vendor.min.css">
     <link rel="stylesheet" href="css/cosmos.min.css">
     <link rel="stylesheet" href="css/application.min.css">
+    <link rel="stylesheet" href="assets/leaflet/leaflet.css">
+    <script src="assets/leaflet/leaflet.js"></script>
   </head>
   <body class="layout layout-header-fixed layout-left-sidebar-fixed">
     <div class="site-overlay"></div>
@@ -40,9 +46,9 @@
             <button class="navbar-toggler left-sidebar-collapse pull-left hidden-xs" type="button">
               <span class="hamburger"></span>
             </button> 
-            <div class="alert alert-light" role="alert">
+            <!-- <div class="alert alert-light" role="alert">
               <center>PETA TRAYEK ANGKUTAN KOTA KABUPATEN GARUT</center>
-            </div>
+            </div> -->
           </div>
         </div>
       </nav>
@@ -56,13 +62,13 @@
             <li>
               <a href="page-layouts.html">
                 <span class="menu-icon">
-                  <i class="zmdi zmdi-home"></i>
+                  <i class="zmdi zmdi-home"></i>  
                 </span>
                 <span class="menu-text">Dashboards</span>
               </a>
             </li>
             <li>
-              <a href="page-layouts.html">
+              <a href="index.php?page=peta">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-view-web"></i>
                 </span>
@@ -70,7 +76,7 @@
               </a>
             </li>  
             <li class="with-sub">
-              <a href="#" aria-haspopup="true">
+              <a href="index.php?page=trayek">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-dot-circle-alt"></i>
                 </span>
@@ -97,7 +103,7 @@
               </ul>
             </li>
             <li class="with-sub">
-              <a href="#" aria-haspopup="true">
+              <a href="index.php?page=angkutan">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-border-all"></i>
                 </span>
@@ -109,7 +115,7 @@
               </ul>
             </li>
             <li>
-              <a href="page-layouts.html">
+              <a href="index.php?page=aplikasi">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-view-web"></i>
                 </span>
@@ -117,7 +123,7 @@
               </a>
             </li> 
             <li>
-              <a href="page-layouts.html">
+              <a href="index.php?page=keluar">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-view-web"></i>
                 </span>
@@ -126,9 +132,23 @@
             </li> 
           </ul>
         </div>
-
       </div>
+      <div class="site-content">
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <?php 
+                $file="page/$page.php";
 
+                if(file_exists("$file")){
+                  include_once($file);
+                }else{
+                  echo "<h3>Halaman Belum dibuat</h3>";
+                }
+            ?>
+          </div>
+        </div>
+        
+      </div>
     </div>
     <script src="js/vendor.min.js"></script>
     <script src="js/cosmos.min.js"></script>
